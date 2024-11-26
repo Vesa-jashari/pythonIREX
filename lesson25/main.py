f29d320d7b1ed21f70c72f0e73b3ba2c8612ab4e
vesa <vesajashari02@gmail.com>
2024-11-25 22:19:29 -0800

    Update main.py

--- lesson25/main.py
+++ lesson25/main.py
@@ -9,25 +9,35 @@ app=FastAPI()
 @app.get("/")
 def read_root():
     return {"message":"Welcome to the Movies CRUD API"}
+
+
 @app.post("/movies/",response_model=models.Movie)
 def create_movie(movie: models.MovieCreate):
     movie_id=database.create_movie(movie)
     return models.Movie(id=movie_id,**movie.dict())
+
+
 @app.get("/movies/",response_model=List[models.Movie])
 def read_movies():
     return database.read_movies()
+
+
 @app.get("/movies/",response_model=models.Movie)
 def read_movie(movie_id:int):
     movie=database.read_movies(movie_id)
     if movie is None:
         raise HTTPException(status_code=404, details="movie not found")
     return movie
+
+
 @app.put("/movies/{movie_id}",response_model=models.Movie)
 def update_movie(movie_id: int, movie:models.MovieCreate):
     updated= database.update_movie(movie_id,movie)
     if not updated:
         raise HTTPException(status_code=404,details="Movie not found")
     return models.Movie(id=movie_id, **movie.dict())
+
+
 @app.delete("/movies/{movie_id}",response_model=dict)
 def delete_movie(movie_id: int):
     deleted=database.delete_movie(movie_id)

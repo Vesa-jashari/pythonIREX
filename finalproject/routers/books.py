b0d06364f82cc45085bbd05da31613271372f187
vesa <vesajashari02@gmail.com>
2024-12-14 21:28:50 +0100

    Update books.py

--- finalproject/routers/books.py
+++ finalproject/routers/books.py
@@ -3,7 +3,7 @@ from typing import List
 
 
 from fastapi import APIRouter, HTTPException, status ,Depends
-from models.book import book, BookCreate
+from models.book import book, BookCreate, Book
 from database import get_db_connection
 from auth.security import get_api_key
 
@@ -34,32 +34,32 @@ def create_book(book:BookCreate, _:str=Depends(get_api_key)):
         genres=",".join(book.genres)
         cursor.execute("insert into books (title, author_id, book_link, genres, average_rating, published_year)"
                        "values (?,?,?,?,?,?"),(book.title, book.author_id, book.book_link, genres,book.average_rating,book.published_year)
-
         conn.commit()
         book_id=cursor.lastrowid
         return Book(id=book_id,**book.dict())
-
-
-
-
-
-
-
-
+    except sqlite3.InternalError:
+        conn.close()
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"Book{book.title} already exists"
+        )
+    finally:
+        conn.close()
 
 @router.put("/{book_id}" ,response_model=Book)
 def update_book(book_id: int, book:BookCreate, _:str=Depends(get_api_key)):
     conn=get_db_connection()
     cursor=conn.cursor()
-    genres=
-
-
-
-
-
-
-
-
+    genres=",".join(book.genres)
+    cursor.execute("Update books set title? , author_id?, book_link=?, genres=?, average_rating=?, published_year=? where id=?", (
+        book.title, book.author_id, book.book_link, genres ,book.average_rating, book.published_year,book_id
+    ))
+    if cursor.rowcount==0:
+        conn.close()
+        raise HTTPException(status_code=404, detail="Book not found")
+    conn.commit()
+    conn.close()
+    return Book(id=book_id, **book.dict())
 
 @router.delete("/{book)id}",response_model=dict)
 def delete_book(book_id:int, _:str=Depends(get_api_key)):

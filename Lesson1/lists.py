c65c6d90f9eab9f3bebe4a32e75ebb97badec829
vesa <vesajashari02@gmail.com>
2024-12-15 20:01:37 +0100

    lesson1

---
+++ Lesson1/lists.py
@@ -0,0 +1,47 @@
+to_do_list = ["math homework", "research for biology", "clean the room", "fun"]
+first_item = to_do_list[0]  # Accesses the first item at index 0, which is "math homework"
+second_item = to_do_list[1]  # Accesses the second item at index 1, which is "research for biology"
+third_item = to_do_list[2]  # Accesses the third item at index 2, which is "clean the room"
+fourth_item = to_do_list[3]  # Accesses the fourth item at index 3, which is "fun"
+
+print(first_item)
+print(second_item)
+print(third_item)
+print(fourth_item)
+
+shopping_list = ["apples", "bananas", 3, 4.5]  # different data types
+
+# Adding "oranges" to the end of the list
+shopping_list.append("oranges")
+print(shopping_list)
+
+# Adding "lemons" to the third position of the list
+shopping_list.insert(2, "lemons")
+
+# Now the list contains ["apples", "bananas", "lemons", 3, 4.5, "oranges"]
+print(shopping_list)
+
+# Removing data from "to_do_list"
+
+# Removing "research for biology" by specifying its value
+to_do_list.remove("research for biology")
+print(to_do_list)
+
+# now the list is ['math homework', 'clean the room', 'fun']
+
+# Removing the item at index 2, which is "fun"
+del to_do_list[2]
+print(to_do_list)
+
+# Update Values
+
+# Changing the first element (index 0 ) from "math homework" to "physics homework"
+to_do_list[0] = "physics homework"
+
+print(to_do_list)  # Output: ['physics homework', 'clean the room']
+
+# 2
+favorite_colors = ["red", "green", "blue", "yellow", "purple"]
+
+fourth_color = favorite_colors[3]  # Accces the fourth color at index 3
+print(fourth_color)
---
+++ Lesson1/variables.py
@@ -0,0 +1,27 @@
+temperature = 98.6
+name = "John"
+
+# Printing the type of our temperature value
+print(type(temperature))
+
+# Printing the type of our name value
+print(type(name))
+
+# Store Data
+age = 30
+print("Age is:", age)
+
+# Perform calculations
+x = 5
+y = 3
+result = x + y  # The result will be 8
+
+# Update values
+age = age + 1  # age is now 31
+print("Age now is:", age)
+
+# Combine Values
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+print(full_name)
